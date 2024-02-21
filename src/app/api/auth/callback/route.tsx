8c58e9147c9ab112e8b7483d347ffb6c703719be
Jan Metelański <metelanskij@gmail.com>
2024-02-21 18:25:41 +0100

    test

--- src/app/api/auth/callback/route.tsx
+++ src/app/api/auth/callback/route.tsx
@@ -1,6 +1,7 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
+import { redirect } from 'next/navigation';
 
 import type { NextRequest } from 'next/server';
 
@@ -18,5 +19,5 @@ export async function GET(request: NextRequest) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(url.origin);
+  return redirect('/');
 }
