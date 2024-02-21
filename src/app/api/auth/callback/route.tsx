10f6e8428a25fab05b3a79c5a897301523d5f16b
Jan Metelański <metelanskij@gmail.com>
2024-02-21 19:59:29 +0100

    a

--- src/app/api/auth/callback/route.tsx
+++ src/app/api/auth/callback/route.tsx
@@ -1,7 +1,6 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
-import { redirect } from 'next/navigation';
 
 import type { NextRequest } from 'next/server';
 
@@ -19,5 +18,5 @@ export async function GET(request: NextRequest) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return redirect('https://freshdesk-clone.vercel.app/login');
+  return NextResponse.redirect(url);
 }
